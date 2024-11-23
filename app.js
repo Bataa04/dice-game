b77f1a09ac5b5c8ba4a14b63b0b2591a57f3019b
bataa <batnyam699@gmail.com>
2024-11-23 21:31:14 +0100

    shoog shiddeg bolgoh

--- app.js
+++ app.js
@@ -6,15 +6,18 @@ var scores = [0, 0];
 var roundScore = 0;
 
 //shooni ali talaaraa buusniig hadgalah huvisagch hregtei,1-6 gesehn utgiig ene huvisagchid sanamsarguigeer uusgej ugnu.
-var dice = Math.floor(Math.random() * 6) + 1;
+var diceNumber = Math.floor(Math.random() * 6) + 1;
 
-//<div class="player-score" id="score-0">43</div>
+// program ehlhed beltgie.
+document.getElementById("score-0").textContent = "0";
+window.document.getElementById("score-1").textContent = "0";
+window.document.getElementById("current-0").textContent = "0";
+window.document.getElementById("current-1").textContent = "0";
 
-//window.document.querySelector("#score-0").textContent = dice;
-//document.querySelector("#score-1").textContent = dice;
-window.document.querySelector("#score-0").textContent = 0;
-window.document.querySelector("#score-1").textContent = 0;
-window.document.querySelector("#current-0").textContent = 0;
-window.document.querySelector("#current-1").textContent = 0;
-document.querySelector(".dice").style.display = "none";
-console.log("Wurfel :" + dice);
+var diceDom = document.querySelector(".dice");
+diceDom.style.display = "none";
+document.querySelector(".btn-roll").addEventListener("click", function () {
+  var diceNumber = Math.floor(Math.random() * 6) + 1;
+  diceDom.style.display = "block";
+  diceDom.src = "dice-" + diceNumber + ".png";
+});
